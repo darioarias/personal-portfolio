09af3f51c8d27d2fc07242d73d61ad1190a2657b
Dario <darioarias12@gmail.com>
2022-07-11 23:34:51 -0400

    update config to act more like a module

--- configs.py
+++ configs.py
@@ -1,31 +1,56 @@
 import secrets
 import os
+from peewee import *
 from dotenv import load_dotenv
 load_dotenv()
 
+
 class Config:
-  SECRET_KEY = secrets.token_hex(255) #creates app's secret key
-  FLASK_COVERAGE = False #Sets Coverage for unit testing and for testing coverage
-  DB_URL=os.getenv('DB_URL')
-  MYSQL_HOST=os.getenv('MYSQL_HOST')
-  MYSQL_USER=os.getenv('MYSQL_USER')
-  MYSQL_PASSWORD=os.getenv('MYSQL_PASSWORD')
-  MYSQL_DATABASE=os.getenv('MYSQL_DATABASE')
+  SECRET_KEY = secrets.token_hex(255)  # creates app's secret key
+  FLASK_COVERAGE = False  # Sets Coverage for unit testing and for testing coverage
+  DB_URL = os.getenv('DB_URL')
+  MYSQL_HOST = os.getenv('MYSQL_HOST')
+  MYSQL_USER = os.getenv('MYSQL_USER')
+  MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+  MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
+
+  @staticmethod
+  def init_app(app):
+    pass
+
 
 class DevelopmentConfig(Config):
-  DEBUG = True #enables debug mode
-  SQLALCHEMY_TRACK_MODIFICATIONS=True
+  DEBUG = True  # enables debug mode
+  SQLALCHEMY_TRACK_MODIFICATIONS = True
+  MYSQL_HOST = "localhost"
+
+  @classmethod
+  def init_app(cls, app):
+    pass
+
+    # check for settings
+
 
 class ProductionConfig(Config):
-  DEBUG = False #enables debug mode
+  DEBUG = False  # enables debug mode
   FLASK_COVERAGE = False
+
+  @classmethod
+  def init_app(cls, app):
+    pass
+
 
 class TestingConfig(Config):
   TESTING = True
   WTF_CSRF_ENABLED = False
-  
+
+  @classmethod
+  def init_app(cls, app):
+    pass
+
+
 config = {
-  'development': DevelopmentConfig,
-  'production': ProductionConfig,
-  'testing': TestingConfig
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
 }
