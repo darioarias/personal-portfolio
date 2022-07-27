082a3723a94f76106fcd35db573cc2893e6e37a1
Dario <darioarias12@gmail.com>
2022-07-26 20:21:05 -0400

    removed the database definition from init into models which allows for dynamic database definition

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ from flask_moment import Moment
 
 bootstrap = Bootstrap()
 moment = Moment()
-db = Proxy()  # Create a proxy for our db.
 
 
 def create_app(config_profile: str):
@@ -23,27 +22,11 @@ def create_app(config_profile: str):
 
   # loads the view(s)
   with app.app_context():
-    from . import models
-
     from .api_v1 import api_v1 as api_v1_blueprint
     app.register_blueprint(api_v1_blueprint, url_prefix='/api/v1')
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
-  if app.config['TESTING']:
-    # print('CREATING TESTING DATABASE')
-    database = SqliteDatabase(
-        'file:memory?mode=memory&cache=shared', uri=False)
-  else:
-    database = MySQLDatabase(
-        os.getenv('MYSQL_DATABASE'),
-        user=os.getenv('MYSQL_USER'),
-        password=os.getenv('MYSQL_PASSWORD'),
-        host=os.getenv('MYSQL_HOST'),
-        port=3306
-    )
-  db.initialize(database)
 
   # returns app instance
   return app
--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from peewee import *
-from flask import current_app
+from flask import current_app as app
 from playhouse.shortcuts import model_to_dict
-from app import db as mydb
 from .validators import TimelinePostValidator
-# creating database connection instance
+import os
+
+mydb_proxy = DatabaseProxy()  # Create a proxy for our db.
 
 
 class TimelinePost(Model):
@@ -14,7 +15,7 @@ class TimelinePost(Model):
   created_at = DateTimeField(default=datetime.utcnow)
 
   class Meta:
-    database = mydb
+    database = mydb_proxy
 
   def validate(self):
     validator = TimelinePostValidator(self)
@@ -29,6 +30,21 @@ class TimelinePost(Model):
   def to_json(self):
     return model_to_dict(self)
 
+
 # TODO: abstract this logic into a context cli function to create all tables, it will be very helpful when deploying
-# mydb.connect();
+# mydb.connect(reuse_if_open=True)
 # mydb.create_tables([TimelinePost])
+
+# dynamically creates the database
+if app.config['TESTING']:
+  database = SqliteDatabase('test.db', uri=True)  # in memory database
+else:
+  database = MySQLDatabase(
+      os.getenv('MYSQL_DATABASE'),
+      user=os.getenv('MYSQL_USER'),
+      password=os.getenv('MYSQL_PASSWORD'),
+      host=os.getenv('MYSQL_HOST'),
+      port=3306
+  )  # connects to database
+
+mydb_proxy.initialize(database)
